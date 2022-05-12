bc13a360f8cdb5ad2d19479797de68426f6146aa
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-05-12 16:29:24 -0300

    Header e footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="index.html">Menu</a></li>
+					<li><a href="sobre.html">Sobre</a></li>
+					<li><a href="contato.html">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © 2022. Alguns direitos reservados.</p>
+		</footer>
+
+    <!-- Footer WordPress -->
+    <?php wp_footer(); ?>
+    <!-- Fecha Footer WordPress -->
+
+	</body>
+</html>
