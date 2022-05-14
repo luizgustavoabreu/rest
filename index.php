418ea80ee8e4a919a067a9a397aebb20736e3b8f
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-05-14 13:12:02 -0300

    Loop
    e correção dos caminhos das imagens nas páginas contato e sobre

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php get_header(); ?>
 
-		<p>Usando Template index</p>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<?php get_footer(); ?>
+		<section class="container sobre">
+			<h2 class="subtitulo"><?php the_title(); ?></h2>
+
+			<div class="grid-8">
+				<?php the_content(); ?>
+			</div>
+		</section>
+
+	<?php endwhile;
+else : ?>
+	<section class="container sobre">
+		<div class="grid-8">
+			<p>Essa página não existe</p>
+		</div>
+	</section>
+	
+<?php endif; ?>
+
+<?php get_footer(); ?>
--- page-home.php
+++ page-home.php
@@ -3,9 +3,10 @@
 ?>
 
 <?php get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<section class="container">
-			<h2 class="subtitulo">Menu da Semana</h2>
+			<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 			<div class="menu-item grid-8">
 				<h2>Peixes</h2>
@@ -63,4 +64,7 @@
 
 		</section>
 
-		<?php get_footer(); ?>
+<?php endwhile;
+else : endif; ?>
+
+<?php get_footer(); ?>
