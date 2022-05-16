92ee83e14f3c5ee70569d41c48b7eaaf0a747ede
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-05-16 18:41:03 -0300

    Pegando valores de outra página

--- header.php
+++ header.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<title><?php bloginfo('name'); ?></title>
 
-		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+<head>
+	<meta charset="utf-8">
+	<title><?php bloginfo('name'); ?></title>
 
-    <!-- Header WordPress -->
-    <?php wp_head(); ?>
-    <!-- Fecha Header WordPress -->
+	<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 
-	</head>
+	<!-- Header WordPress -->
+	<?php wp_head(); ?>
+	<!-- Fecha Header WordPress -->
 
-	<body>
-		
-		<header>
-			<nav>
-				<ul>
-					<li class="current_page_item"><a href="/">Menu</a></li>
-					<li><a href="/sobre">Sobre</a></li>
-					<li><a href="/contato">Contato</a></li>
-				</ul>
-			</nav>
+</head>
 
-			<h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
+<body>
 
-			<p>Rua Marechal 29 – Copacabana – Rj</p>
-			<p class="telefone">2422-9201</p>
-		</header>
+	<header>
+		<nav>
+			<ul>
+				<li class="current_page_item"><a href="/">Menu</a></li>
+				<li><a href="/sobre">Sobre</a></li>
+				<li><a href="/contato">Contato</a></li>
+			</ul>
+		</nav>
+
+		<h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
+
+		<?php $contato = 29; ?>
+		<p><?php the_field('endereco_header', $contato); ?></p>
+		<p class="telefone"><?php the_field('telefone_header', $contato); ?></p>
+
+	</header>
