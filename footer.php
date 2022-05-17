cdc5800c92a1b504278d1354d54073a6cff01fb3
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-05-17 19:02:30 -0300

    Adicionando mais campos personalizados e de repetição

--- footer.php
+++ footer.php
@@ -1,18 +1,19 @@
 <footer>
-			<nav class="nav-footer">
-				<ul>
-					<li class="current_page_item"><a href="/">Menu</a></li>
-					<li><a href="/sobre">Sobre</a></li>
-					<li><a href="/contato">Contato</a></li>
-				</ul>
-			</nav>
+	<nav class="nav-footer">
+		<ul>
+			<li class="current_page_item"><a href="/">Menu</a></li>
+			<li><a href="/sobre">Sobre</a></li>
+			<li><a href="/contato">Contato</a></li>
+		</ul>
+	</nav>
 
-			<p>Rest © 2022. Alguns direitos reservados.</p>
-		</footer>
+	<p><?php bloginfo('name'); ?> © <?php echo date("Y"); ?> - Alguns direitos reservados.</p>
+</footer>
 
-    <!-- Footer WordPress -->
-    <?php wp_footer(); ?>
-    <!-- Fecha Footer WordPress -->
+<!-- Footer WordPress -->
+<?php wp_footer(); ?>
+<!-- Fecha Footer WordPress -->
 
-	</body>
+</body>
+
 </html>
--- page-sobre.php
+++ page-sobre.php
@@ -1,8 +1,8 @@
 <?php
 // Template Name: Sobre
 ?>
-
 <?php get_header(); ?>
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<section class="container sobre">
