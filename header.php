e16592d81fe15138f8d3b8d7a07930add0f9c9c0
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-05-18 12:00:39 -0300

    Adicionando campos personalizados para SEO

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
 <head>
 
 	<meta charset="utf-8">
-	<title><?php bloginfo('name'); ?></title>
+	<title><?php bloginfo('name'); ?> | <?php wp_title(''); ?> <?php the_field('title_seo'); ?></title>
+	<meta name="description" content="<?php bloginfo('name'); ?> | <?php wp_title(''); ?> <?php the_field('description_seo'); ?>">
 
 	<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
